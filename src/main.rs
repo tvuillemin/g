af89b724f493f9f20a8324a59bf0be4e02c132b6
Thibaut Vuillemin <thibaut.vuillemin@kraken.tech>
2025-12-12 22:46:04 +0100

    feat: add basic short commands

--- src/main.rs
+++ src/main.rs
@@ -5,16 +5,29 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     if args.len() <= 1 {
-        run_git(&["--help".to_string()]);
+        run_git(&["--help"]);
         return;
     }
 
-    let git_args = &args[1..];
+    let str_args: &[&str] = &args[1..].iter().map(|s| s.as_str()).collect::<Vec<_>>();
 
-    run_git(git_args);
+    let git_args: Vec<&str> = match str_args {
+        ["ca"] => vec!["commit", "--amend"],
+        ["co"] => vec!["checkout"],
+        ["st"] => vec!["status"],
+        ["log"] => vec![
+            "log",
+            "--graph",
+            "--oneline",
+        ],
+
+        other => other.to_vec(),
+    };
+
+    run_git(&git_args);
 }
 
-fn run_git(git_args: &[String]) {
+fn run_git(git_args: &[&str]) {
     let status = Command::new("git")
         .args(git_args)
         .stdin(Stdio::inherit())
