801b9c776bd27579097bcba39104ec62d54298f5
Thibaut Vuillemin <thibaut.vuillemin@kraken.tech>
2025-12-19 20:43:17 +0100

    refactor: improve OS interactions

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,30 @@
-use std::env;
 use std::process::{Command, Stdio};
+use std::env;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    let args: Vec<String> = env::args().skip(1).collect();
+    let args_str: Vec<&str> = args.iter().map(|s| s.as_str()).collect();
 
-    if args.len() <= 1 {
-        run_git(&["--help"]);
-        return;
-    }
-
-    let str_args: &[&str] = &args[1..].iter().map(|s| s.as_str()).collect::<Vec<_>>();
-
-    let git_args: Vec<&str> = match str_args {
-        ["ca"] => vec!["commit", "--amend"],
-        ["co"] => vec!["checkout"],
-        ["pf"] => vec!["push", "--force-with-lease"],
-        ["pr"] => vec!["pull", "--rebase"],
-        ["st"] => vec!["status"],
-        ["log"] => vec![
-            "log",
-            "--graph",
-            "--oneline",
-        ],
-
-        other => other.to_vec(),
+    match args_str.as_slice() {
+        [] => run_git(&["--help"]),
+        ["ca"] => run_git(&["commit", "--amend"]),
+        ["co"] => run_git(&["checkout"]),
+        ["log"] => run_git(&["log", "--graph", "--oneline"]),
+        ["pf"] => run_git(&["push", "--force-with-lease"]),
+        ["pr"] => run_git(&["pull", "--rebase"]),
+        ["st"] => run_git(&["status"]),
+        other => run_git(other),
     };
-
-    run_git(&git_args);
 }
 
-fn run_git(git_args: &[&str]) {
-    let status = Command::new("git")
-        .args(git_args)
+fn run_git(args: &[&str]) {
+    let mut cmd = Command::new("git");
+    let status = cmd.args(args)
         .stdin(Stdio::inherit())
         .stdout(Stdio::inherit())
         .stderr(Stdio::inherit())
-        .status()
-        .expect("❌ Could not run `git`. Make sure Git is installed and in the PATH.");
-
-    std::process::exit(status.code().unwrap_or(1));
+        .status().expect("❌ Could not run git");
+    if !status.success() {
+        std::process::exit(status.code().unwrap_or(1));
+    }
 }
