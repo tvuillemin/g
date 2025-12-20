2e7e59bd13feea54598a8f6ad4f5240a64a3183d
Thibaut Vuillemin <thibaut.vuillemin@kraken.tech>
2025-12-20 14:23:15 +0100

    feat: post merge

--- src/commands.rs
+++ src/commands.rs
@@ -1,8 +1,17 @@
-use crate::utils::{get_trunk, run_git};
+use crate::utils::{get_current_branch, get_trunk, run_git};
 
 pub fn branch_update() {
     let trunk = get_trunk();
     run_git(&["fetch", "-p"]);
     run_git(&["rebase", &format!("origin/{}", trunk)]);
     run_git(&["push", "--force-with-lease", "--no-verify"]);
+}
+
+pub fn post_merge() {
+    let branch_to_be_deleted = get_current_branch();
+    let trunk = get_trunk();
+
+    run_git(&["checkout", &trunk]);
+    run_git(&["pull", "--rebase"]);
+    run_git(&["branch", "-d", &branch_to_be_deleted]);
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 mod commands;
 mod utils;
 
-use commands::branch_update;
+use commands::{branch_update, post_merge};
 use std::env;
 use utils::run_git;
 
@@ -10,14 +10,15 @@ fn main() {
     let args_str: Vec<&str> = args.iter().map(|s| s.as_str()).collect();
 
     match args_str.as_slice() {
-        [] => run_git(&["--help"]),
         ["bu"] => branch_update(),
         ["ca"] => run_git(&["commit", "--amend"]),
         ["co"] => run_git(&["checkout"]),
         ["log"] => run_git(&["log", "--graph", "--oneline"]),
         ["pf"] => run_git(&["push", "--force-with-lease"]),
+        ["pm"] => post_merge(),
         ["pr"] => run_git(&["pull", "--rebase"]),
         ["st"] => run_git(&["status"]),
+        [] => run_git(&["--help"]),
         other => run_git(other),
     };
 }
