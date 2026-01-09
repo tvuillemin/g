6544256386e747b32e5badc50ca27a5b5c56dcf6
Thibaut Vuillemin <thibaut.vuillemin@kraken.tech>
2026-01-09 12:29:59 +0100

    fix: g co

--- src/main.rs
+++ src/main.rs
@@ -3,22 +3,35 @@ mod utils;
 
 use commands::{branch_update, post_merge};
 use std::env;
-use utils::run_git;
+use utils::{get_trunk, run_git};
 
 fn main() {
     let args: Vec<String> = env::args().skip(1).collect();
-    let args_str: Vec<&str> = args.iter().map(|s| s.as_str()).collect();
 
-    match args_str.as_slice() {
-        ["bu"] => branch_update(),
-        ["ca"] => run_git(&["commit", "--amend"]),
-        ["co"] => run_git(&["checkout"]),
-        ["log"] => run_git(&["log", "--graph", "--oneline"]),
-        ["pf"] => run_git(&["push", "--force-with-lease"]),
-        ["pm"] => post_merge(),
-        ["pr"] => run_git(&["pull", "--rebase"]),
-        ["st"] => run_git(&["status"]),
-        [] => run_git(&["--help"]),
-        other => run_git(other),
+    if args.is_empty() {
+        run_git(&["--help"]);
+        return;
+    }
+
+    let (main_arg, extra_args) = args.split_first().unwrap();
+
+    match main_arg.as_str() {
+        "bu" => branch_update(),
+        "ca" => run_git(&["commit", "--amend"]),
+        "co" => {
+            let mut git_args = vec!["checkout"];
+            git_args.extend(extra_args.iter().map(|s| s.as_str()));
+            run_git(&git_args);
+        }
+        "log" => run_git(&["log", "--graph", "--oneline"]),
+        "pf" => run_git(&["push", "--force-with-lease"]),
+        "pm" => post_merge(),
+        "pr" => run_git(&["pull", "--rebase"]),
+        "st" => run_git(&["status"]),
+        _ => {
+            let mut git_args = vec![main_arg.as_str()];
+            git_args.extend(extra_args.iter().map(|s| s.as_str()));
+            run_git(&git_args);
+        }
     };
 }
